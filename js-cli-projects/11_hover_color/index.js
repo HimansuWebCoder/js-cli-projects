1b49d0d615f672ad463e7150cab1afdd56c9b54a
himansu naik <naikhimansu546@gmail.com>
2025-07-07 10:21:43 +0530

    updated

---
+++ js-cli-projects/11_hover_color/index.js
@@ -0,0 +1,119 @@
+for (let i = 0; i < 5; i++) {
+	for (let j = 0; j < 5; j++) {
+		console.log(j);
+	}
+}
+
+
+
+let i = 0;
+
+if (i < 5) {
+	 let j = 0;
+	    if (j < 5) {
+	    	console.log(j); // 0
+	    }
+	    j++;
+	    if ( j < 5) {
+	    	console.log(j); // 1
+	    }
+	    j++;
+	    if (j < 5) {
+	    	console.log(j); // 2
+	    }
+	    j++;
+	    if (j < 5) {
+	    	console.log(j); // 3
+	    }
+	    j++;
+	    if (j < 5) {
+	    	console.log(j); // 4
+	    }
+}
+
+i++;
+// i = 1;
+
+if (i < 5) {
+	let j = 0;
+	  if (j < 5) {
+	  	console.log(j); 
+	  }
+}
+
+// let i = 0;
+// if (i < 5) {
+// 	let j = 0;
+// 	   if (j < 5) {
+// 	   	 console.log(i); // 0
+// 	   }
+// 	J++
+// 	 j = 1;
+// 	 if (j < 5) {
+// 	 	console.log(i); // 0
+// 	 }
+// 	j++
+// 	j = 2;
+// 	if (j < 5) {
+// 		console.log(i); // 0
+// 	}
+// 	j++;
+// 	j = 3;
+// 	if (j < 5) {
+// 		console.log(i); // 0
+// 	}
+// 	j++;
+// 	j = 4;
+// 	if (j < 5) {
+// 		console.log(i); // 0
+// 	}
+// }
+
+// // then jump to i++; i = 1;
+// i++;
+// i = 1;
+
+// if (i < 5) {
+
+// }
+
+// let i = 0;
+// if (i < 5) {
+// 	let j = 0;
+// 	   if (j < 5) {
+// 	   	 console.log(j); // 0
+// 	   }
+// 	J++
+// 	 j = 1;
+// 	 if (j < 5) {
+// 	 	console.log(j); // 1
+// 	 }
+// 	j++
+// 	j = 2;
+// 	if (j < 5) {
+// 		console.log(j); // 2
+// 	}
+// 	j++;
+// 	j = 3;
+// 	if (j < 5) {
+// 		console.log(j); // 3
+// 	}
+// 	j++;
+// 	j = 4;
+// 	if (j < 5) {
+// 		console.log(j); // 4
+// 	}
+// }
+
+// i++;
+// let i = 1;
+
+// if (i < 5) {
+// 	let j = 0;
+// 	   if (j < 5) {
+// 	   	console.log(j); // 
+// 	   }
+// }
+
+
+
