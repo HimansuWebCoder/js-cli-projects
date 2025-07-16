f405c648a44b17546cbede20d125733f1ec1a41e
himansu naik <naikhimansu546@gmail.com>
2025-07-16 17:22:05 +0530

    add star pattern printing and update

---
+++ js-cli-projects/12_start_pattern_printing/index.js
@@ -0,0 +1,26 @@
+// Start pattern printing
+
+let star = "";
+
+for (let i = 0; i < 10; i++) {
+	// star += "*";
+	// star += ` adu`;
+	star += ` 🌸`;
+	// star += ` Krishna 🌸`;
+	// star += " " +"krishna";
+	console.log(star);
+}
+
+// Reverse star pattern printing
+for (let i = 10; i > 0; i--) {
+  let star = "";
+   for (let j = 0; j < i; j++) {
+   	 // star+= "*";
+   	 // star+= "adu";
+   	star += ` 🌸`;
+   }
+   console.log(star);
+}
+
+// coding is so much fun if use emojis, or names or anything playful like game or like that
+
